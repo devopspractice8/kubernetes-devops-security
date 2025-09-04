c139dc2c51977934124492a4b20e6fdc5d6ab968
devopspractice8 <ayubshaikh9226@gmail.com>
2025-09-04 16:01:07 +0530

    Update install-script.sh

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -9,7 +9,7 @@ source ~/.bashrc
 # Don't ask to restart services after apt update, just do it.
 [ -f /etc/needrestart/needrestart.conf ] && sed -i 's/#\$nrconf{restart} = \x27i\x27/$nrconf{restart} = \x27a\x27/' /etc/needrestart/needrestart.conf
 
-apt-get autoremove -y  #removes the packages that are no longer needed
+apt-get autoremove -y
 apt-get update
 systemctl daemon-reload
 
@@ -23,8 +23,7 @@ KUBE_VERSION=$(apt-cache madison kubeadm | head -1 | awk '{print $3}')
 apt-get install -y docker.io vim build-essential jq python3-pip kubelet kubectl kubernetes-cni kubeadm containerd
 pip3 install jc
 
-### UUID of VM
-### comment below line if this Script is not executed on Cloud based VMs
+# UUID of VM
 jc dmidecode | jq .[1].values.uuid -r
 
 systemctl enable kubelet
@@ -37,9 +36,8 @@ mkdir -p /etc/containerd
 containerd config default | sed 's/SystemdCgroup = false/SystemdCgroup = true/' > /etc/containerd/config.toml
 systemctl restart containerd
 
-# uncomment below line if your host doesnt have minimum requirement of 2 CPU
-# kubeadm init --pod-network-cidr '10.244.0.0/16' --service-cidr '10.96.0.0/16' --ignore-preflight-errors=NumCPU --skip-token-print
-kubeadm init --pod-network-cidr '10.244.0.0/16' --service-cidr '10.96.0.0/16'  --skip-token-print
+# kubeadm init
+kubeadm init --pod-network-cidr '10.244.0.0/16' --service-cidr '10.96.0.0/16' --skip-token-print
 
 mkdir -p ~/.kube
 cp -i /etc/kubernetes/admin.conf ~/.kube/config
@@ -50,14 +48,12 @@ sleep 5
 
 echo "untaint controlplane node"
 node=$(kubectl get nodes -o=jsonpath='{.items[0].metadata.name}')
-for taint in $(kubectl get node $node -o jsonpath='{range .spec.taints[*]}{.key}{":"}{.effect}{"-"}{end}')
-do
+for taint in $(kubectl get node $node -o jsonpath='{range .spec.taints[*]}{.key}{":"}{.effect}{"-"}{end}'); do
     kubectl taint node $node $taint
 done
 kubectl get nodes -o wide
 
 echo ".........----------------#################._.-.-Docker-.-._.#################----------------........."
-
 cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
@@ -71,9 +67,8 @@ systemctl daemon-reload
 systemctl restart docker
 systemctl enable docker
 
-
-echo ".........----------------#################._.-.-Java and MAVEN-.-._.#################----------------........."
-apt install openjdk-11-jdk maven -y
+echo ".........----------------#################._.-.-Java 17 and MAVEN-.-._.#################----------------........."
+apt install openjdk-17-jdk maven -y
 java -version
 mvn -v
 
